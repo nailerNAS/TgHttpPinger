324b567da542334bf9a89c8f690bb0e437748c93
nailer <nailer@pm.me>
2018-11-27 18:00:08 +0200

    add pingresult class

---
+++ TgHttpPinger/Pinger/PingResult.cs
@@ -0,0 +1,10 @@
+﻿namespace TgHttpPinger.Pinger
+{
+    public class PingResult
+    {
+        public string Url;
+        public int StatusCode;
+        public float Delay;
+        public bool IsTimedOut;
+    }
+}
