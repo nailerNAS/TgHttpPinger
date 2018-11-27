8c7d3ddaac90cee76f6508777f34984afdc75e14
nailer <nailer@pm.me>
2018-11-27 18:17:00 +0200

    add unit test for pinger

---
+++ TgHttpPingerTests/Pinger/PingerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TgHttpPinger.Pinger.Tests
+{
+    [TestClass()]
+    public class PingerTests
+    {
+        const string url = @"http://httpbin.org/";
+
+        [TestMethod()]
+        public void PingTest()
+        {
+            PingResult pingResult = Pinger.Ping(url).Result;
+
+            Assert.IsFalse(pingResult.IsTimedOut);
+
+            Console.WriteLine(
+                $"{pingResult.Delay}\n" +
+                $"{pingResult.IsTimedOut}\n" +
+                $"{pingResult.StatusCode}\n" +
+                $"{pingResult.Url}");
+        }
+
+        [TestMethod()]
+        public void IsValidUrlTest()
+        {
+            Assert.IsTrue(Pinger.IsValidUrl(url));
+            Assert.IsFalse(Pinger.IsValidUrl(""));
+        }
+    }
+}
